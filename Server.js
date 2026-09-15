423834862302ecbf754c520066df641ed93d4801
Xenkora Technologies <xenkoratechnologies@gmail.com>
2026-09-16 03:13:25 +0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -7,7 +7,10 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://xenkora-ai-platform.vercel.app"
+    ],
     credentials: true,
   }),
 );
