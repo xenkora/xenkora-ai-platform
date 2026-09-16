03a649d17d0abc96dde0da2a0fafc324e7d451e7
Xenkora Technologies <xenkoratechnologies@gmail.com>
2026-09-16 19:27:27 +0500

    Delete Routes directory

--- Routes/ContactRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createContact } from "../Controllers/ContactController.js";
-
-const router = express.Router();
-
-router.post("/create-contact", createContact);
-
-export default router;
